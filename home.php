0a313d4ad8d6ca6ef12006c093ecb5136c070cdf
Palakrustagi <palakruatagi10@gmail.com>
2022-02-22 10:32:20 +0530

    adding game

--- home.php
+++ home.php
@@ -1,20 +1,11 @@
 <html>
     <head>
         <title>Home</title>
-        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/css/bootstrap.min.css">
+        <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
 
-<!-- jQuery library -->
-<script src="https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.slim.min.js"></script>
-
-<!-- Popper JS -->
-<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"></script>
-
-<!-- Latest compiled JavaScript -->
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/js/bootstrap.bundle.min.js"></script>
-<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css">
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/js/bootstrap.min.js"></script>
-<link rel="stylesheet" href="home.css">
+        <link rel="stylesheet" href="home.css?v=<?php echo time(); ?>">
     </head>
     <body>
         <ul>
@@ -22,31 +13,27 @@
             <li><a href="logout.php">Logout</a></li>
            <li> <a href="active.php">Active <span class="glyphicon glyphicon-user"></span></a></li>
            </ul>
-               <div class="box">
-                   <div class="container">
+           <div class="box">
+           <h1 style="color: rgb(94, 94, 253);font-size:50px;"><b>WELCOME</b></h1>
+           
+           </div>
+           <div class= "game">
+            <div class="row">
+                <div class="col-lg-4" >
                     
-                       
-                 
-                 <?php
 
-                  session_start();
-                  
-                  $host="localhost";
-                  $us="root";
-                  $pass="";
-                  $con=mysqli_connect("$host","$us",$pass);
-                  
-                  mysqli_select_db($con,"registrationdata");
-                 
-                  echo "<b> WELCOME:   ". $_SESSION['USERNAME'] ." </b>  <br>";
+                <img src="ttt.jpg" class="img-circle" alt="tictactoe" height="300px" width="250px">
+                <a href="game.html" class="btn btn-primary" class="btn" role="button">Play Now</a>
+                </div>
+                
+                </div>
+            </div>
+                
 
-                 ?>
-               </div>
-               </div>
+           
+           
 
-
-     <p>PALAK</p>
-        
+          
            
            
             
